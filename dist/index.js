55871ace1d59bf4750dc5da8ec577fb59bc61d7f
softyAmir <theyabcpssouaf2@gmail.com>
2024-01-07 00:42:29 -1100

    connecting mongo db

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,9 @@
 import express from "express";
 const app = express();
-
 app.get("/", (req, res) => {
     res.send("HELLO Blud");
 });
-
-console.log("ccd");
-
+console.log("cc");
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`the server is running on port ${PORT}.../`);
