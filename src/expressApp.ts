d3e3faaf9e39372248c79b3d227d90ac8bd91f5d
Amir SouaF <111384104+AmlrSF@users.noreply.github.com>
2024-01-10 11:53:23 -1100

    Update expressApp.ts

--- src/expressApp.ts
+++ src/expressApp.ts
@@ -6,7 +6,9 @@ import { Express } from "express-serve-static-core";
 export const  expressApp =  async (app: Express) =>{
 
     app.use(express.json({ limit: '1mb'}));
+    
     app.use(express.urlencoded({ extended: true, limit: '1mb'}));
+    
     app.use(cors());
 
 
